b050e001b06d751d9530fd26692848fe76d28262
Yarkeev Denis <d.yarkeev@corp.mail.ru>
2023-08-22 07:49:18 +0300

    export active user from rest

--- rest/src/index.ts
+++ rest/src/index.ts
@@ -4,6 +4,7 @@ export * from '@enums';
 export * from '@node/NerveNodeObject';
 
 export * from './NerveRestAction';
+export * from './NerveRestActiveUser';
 export * from './NerveRestApp';
 export * from './NerveRestContext';
 export * from './NerveRestContextParam';
