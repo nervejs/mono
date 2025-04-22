1b137031cc693a31ecd47cbc6e1875cf807404ba
gennadiy.kondratev <gennadiy.kondratev@vk.team>
2025-04-22 20:32:28 +0300

    VKPL-10123: fix MR

--- server/src/NerveServerRequest/NerveServerRequest.ts
+++ server/src/NerveServerRequest/NerveServerRequest.ts
@@ -142,24 +142,26 @@ export class NerveServerRequest extends NerveNodeObject {
 		const method = String(this.params.method || 'GET').toUpperCase();
 		const duration = this.endTimestamp - this.startTimestamp;
 
-		if (this.options.log.response.status) {
-			this.logInfo(`Response Status: ${this.response.status}`);
-		}
-
-		if (this.options.log.response.headers) {
-			this.logInfo(`Response Headers: ${JSON.stringify(this.response.headers)}`);
-		}
-
-		if (this.options.log.response.body) {
-			this.logInfo(`Response Body: ${this.response.raw}`);
-		}
-
 		if (this.options.log.duration) {
 			this.logDebug(`Duration: ${duration}ms`);
 		}
 
-		if (this.options.log.summary) {
-			this.logInfo(`${method} ${this.getRequestUrl()} ${this.response.status} ${duration}ms`);
+		if (this.response) {
+			if (this.options.log.response.status) {
+				this.logInfo(`Response Status: ${this.response.status}`);
+			}
+
+			if (this.options.log.response.headers) {
+				this.logInfo(`Response Headers: ${JSON.stringify(this.response.headers)}`);
+			}
+
+			if (this.options.log.response.body) {
+				this.logInfo(`Response Body: ${this.response.raw}`);
+			}
+
+			if (this.options.log.summary) {
+				this.logInfo(`${method} ${this.getRequestUrl()} ${this.response.status} ${duration}ms`);
+			}
 		}
 	}
 
@@ -238,10 +240,10 @@ export class NerveServerRequest extends NerveNodeObject {
 
 		try {
 			this.response = await this.send<R>();
-			this.afterFetch();
 		} catch (err) {
 			error = err;
-			this.logError('Request failed', err as Error);
+		} finally {
+			this.afterFetch();
 		}
 
 		if (error) {
